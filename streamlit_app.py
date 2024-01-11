cac847a5669144bfff86f635bd959ad382c2cea0
Bokluk2 <156343972+Bokluk2@users.noreply.github.com>
2024-01-11 18:45:02 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ streamlit.header('Breakfast Menu 🥣 🥗 🐔 🥑🍞')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')

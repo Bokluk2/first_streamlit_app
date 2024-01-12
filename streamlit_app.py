e09c4818114c32c5334b8aa6249b977485b70c8f
Bokluk2 <156343972+Bokluk2@users.noreply.github.com>
2024-01-12 15:06:03 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,3 +29,8 @@ streamlit.dataframe(fruityvice_normalized)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
+
+fruit_choice=srtreamlit.text_input('What fruit?','kiwi')
+streamlit.write('User entered:',fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+

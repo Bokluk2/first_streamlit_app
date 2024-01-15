b9ccadace40cebc7ef7d5bea72f5897b6dc50db9
Bokluk2 <156343972+Bokluk2@users.noreply.github.com>
2024-01-15 11:02:16 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,6 +64,7 @@ streamlit.write('The user entered ', fruit_choice)
 streamlit.stop()
 
 streamlit.header("Fruit load list contains:")
+streamlit.write('This is the new section')
 #SF - related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:

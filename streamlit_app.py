fa45c42d8bcfb5bd8dbf275c47b0dc5ba9809910
Bokluk2 <156343972+Bokluk2@users.noreply.github.com>
2024-01-15 10:31:20 +0200

    Update streamlit_app.py
    try-except

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,13 +35,24 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
+#New
+try:
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+if not fruit_choice
+  streamlit.error("Please select a fruit.")
+else
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+  fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
+  streamlit.dataframe(fruityvice_normalized)
+
+except:
+  URLError as e:
+  streamlit.error();
+  
 streamlit.write('The user entered ', fruit_choice)
 
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
+
 
 #Lesson 12
 
